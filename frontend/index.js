31dbda624ebdfa040e7300028fbbc25d218349ea
harshitha888923 <24wh1a05v1@bvrihhyderabad.edu.in>
2026-07-03 19:28:28 +0530

    feat: add express as a dependency in package.json

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,23 @@
-console.log("hello ")
-/*alert("hello")
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "frontend/img.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "frontend/john.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+}
